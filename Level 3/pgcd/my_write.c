1c21f295d9a24dae8263ba08be412ab4b2421b10
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-10-06 01:25:48 +0300

    Update my_write.c

--- Level 3/pgcd/my_write.c
+++ Level 3/pgcd/my_write.c
@@ -1,25 +1,27 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int main(int ac, char const **av)
-{
-	int nbr1;
-	int nbr2;
-
-	if (ac == 3)
-	{
-		if ((nbr1 = atoi(av[1])) > 0 && (nbr2 = atoi(av[2])) > 0)
-		{
-			while (nbr1 != nbr2)
-			{
-				if (nbr1 > nbr2)
-					nbr1 -= nbr2;
-				else
-					nbr2 -= nbr1;
-			}
-			printf("%d", nbr1);
-		}
-	}
-	printf("\n");
-	return (0);
+int main(int ac, char **av) {
+    if (ac == 3)
+    {
+        int n1 = atoi(av[1]);
+        int n2 = atoi(av[2]);
+        if (n1 > 0 && n2 > 0)
+        {
+            while (n1 != n2)
+            {
+                if (n1 > n2)
+                {
+                    n1 -= n2;
+                }
+                else
+                {
+                    n2 -= n1;
+                }
+            }
+            printf("%d", n1);
+        }
+    }
+    printf("\n");
+    return (0);
 }
