c94a81bd11e1c6e6b9df3d6ae0084819239c5fb1
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-10-05 17:01:18 +0300

    Update my_write.c

--- Level 3/epur_str/my_write.c
+++ Level 3/epur_str/my_write.c
@@ -1,29 +1,29 @@
 #include <unistd.h>
 
-void epur_str(char *str)
+int	 main(int ac, char const **av)
 {
-	char sp = -1;
-	int i = 0;
+	int i;
+	int flg = 0;
 
-	while(str[i])
-	{
-		if (str[i] != ' ' && str[i] != '\t')
-		{
-			if (sp == 1)
-				write(1, " ", 1);
-			sp = 0;
-			write(1, &str[i], 1);
-		}
-		else if (sp == 0)
-			sp = 1;
-		i++;
-	}
-}
-
-int main(int ac, char **av)
-{
 	if (ac == 2)
-		epur_str(av[1]);
+	{
+		i = 0;
+		while (av[1][i] == ' ' || av[1][i] == '\t')
+			i++;
+		while (av[1][i])
+		{
+			if (av[1][i] == ' ' || av[1][i] == '\t')
+				flg = 1;
+			else
+			{
+				if (flg)
+					write(1, " ", 1);
+				flg = 0;
+				write(1, &av[1][i], 1);
+			}
+			i++;
+		}
+	}
 	write(1, "\n", 1);
 	return (0);
 }
