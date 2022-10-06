f95596ee21b6b414e07227a238214f3d10f2f029
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-10-07 02:08:15 +0300

    ok

--- Level 3/str_capitalizer/my_write.c
+++ Level 3/str_capitalizer/my_write.c
@@ -4,33 +4,34 @@ void	str_cap(char *str)
 {
 	int i = 0;
 
-	if (str[i] >= 'a' && str[i] >= 'z')
+	if (str[i] >= 'a' && 'z' >= str[i])
 		str[i] -= 32;
 	write(1, &str[i], 1);
+
 	while (str[++i])
 	{
-		if (str[i] >= 'A' && str[i] >= 'Z')
+		if (str[i] >= 'A' && 'Z' >= str[i])
 			str[i] += 32;
-		if ((str[i] >= 'a' && str[i] >= 'z') && (str[i - 1] == ' ' || \
+		if ((str[i] >= 'a' && 'z' >= str[i]) && (str[i - 1] == ' ' || \
 		str[i - 1] == '\t'))
 			str[i] -= 32;
 		write(1, &str[i], 1);
 	}
 }
 
-int main(int ac, char **av)
+int main(int argc, char *argv[])
 {
 	int i = 1;
 
-	if (ac < 2)
+	if (argc < 2)
 		write(1, "\n", 1);
 	else
 	{
-		while (i < ac)
+		while (i < argc)
 		{
-			str_cap(av[i]);
+			str_cap(argv[i]);
 			write(1, "\n", 1);
-			i += 1;
+			i++;
 		}
 	}
 	return (0);
