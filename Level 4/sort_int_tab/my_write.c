54d107ccb2138d50cc1225381c9dcfe456c2e36b
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-10-08 02:44:24 +0300

    ok

---
+++ Level 4/sort_int_tab/my_write.c
@@ -0,0 +1,24 @@
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	int				tmp;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				tmp = 0;
+				tmp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
