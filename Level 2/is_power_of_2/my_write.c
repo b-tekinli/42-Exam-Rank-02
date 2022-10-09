b0b4fd006019b8781ec44ffbd9eb14e827b05bc2
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-10-09 12:23:38 +0300

    ok

--- Level 2/is_power_of_2/my_write.c
+++ Level 2/is_power_of_2/my_write.c
@@ -1,7 +1,7 @@
-int	    is_power_of_2(unsigned int n) {
-    if(n == 0)
-        return (0);
-    if(n % 2 == 0)
-        n /= 2;
-    return ((n == 1) ? 1 : 0);
+int is_power_of_2(unsigned int n)
+{
+	if (n == 0)
+		return (0);
+	else
+		return ((n & (-n)) == n ? 1: 0);
 }
