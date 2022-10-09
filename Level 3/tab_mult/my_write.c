f69a8483122d1d964213b506adc770176ba9787e
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-10-09 15:53:46 +0300

    ok

--- Level 3/tab_mult/my_write.c
+++ Level 3/tab_mult/my_write.c
@@ -1,75 +1,76 @@
 #include <unistd.h>
 
 int ft_atoi(char *str) {
+    int i = 0;
     int n = 1;
     int result = 0;
 
-    if (*str == ' ' || (*str >= 9 && *str <= 13))
+    if (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
     {
-        str++;
+        i++;
     }
-    if (*str == '-')
-	{
-		n = -1;
-	}
-	if (*str == '-' || *str == '+')
-	{
-		str++;
-	}
-	while (*str && *str >= '0' && *str <= '9')
-	{
-		result = result * 10 + *str - '0';
-		str++;
-	}
-	return (result * n);
+    if (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+        {
+            n = -1;
+        }
+        i++;
+    }
+    while (str[i] && str[i] >= '0' && str[i] <= '9')
+    {
+        result = result * 10 + str[i] - '0';
+        i++;
+    }
+    return (result * n);
 }
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
+void ft_putchar(char c) {
+    write(1, &c, 1);
 }
 
-void	ft_putnbr(int nb)
-{
-	char c;
+void ft_putnbr(int n) {
+    char c;
 
-	if (nb < 0)
-	{
-		nb = -nb;
-		ft_putchar('-');
-	}
-	if (nb < 10)
-	{
-		c = nb + '0';
-		write(1, &c, 1);
-	}
-	else
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-	}
+    if (n < 0)
+    {
+        n = -n;
+        write(1, "-", 1);
+    }
+    if (n < 10)
+    {
+        c = n + '0';
+        write(1, &c, 1);
+    }
+    else
+    {
+        ft_putnbr(n / 10);
+        ft_putnbr(n % 10);
+    }
 }
 
-void multi(int nb)
-{
-	int	i = 1;
-	while (i <= 9)
-	{
-		ft_putnbr(i);
-		write(1, " x ", 3);
-		ft_putnbr(nb);
-		write(1, " = ", 3);
-		ft_putnbr(i * nb);
-		ft_putchar('\n');
-		i++;
-	}
+void print_table(int n) {
+    int i = 1;
+
+    while (i <= 9)
+    {
+        ft_putnbr(i);
+        write(1, " x ", 3);
+        ft_putnbr(n);
+        write(1, " = ", 3);
+        ft_putnbr(i * n);
+        write(1, "\n", 1);
+        i++;
+    }
 }
 
-int	main(int ac, char **av)
-{
-	if (ac == 2)
-		multi(ft_atoi(av[1]));
-	else
-		ft_putchar('\n');
-	return (0);
+int main(int ac, char **av) {
+    if (ac == 2)
+    {
+        print_table(ft_atoi(av[1]));
+    }
+    else {
+        write(1, "\n", 1);
+    }
+    return (0);
 }
