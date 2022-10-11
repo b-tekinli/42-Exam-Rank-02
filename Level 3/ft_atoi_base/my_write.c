0bdaa5c00332a4a815509ae9e5506d92581efbc4
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-10-11 21:12:02 +0300

    ok

--- Level 3/ft_atoi_base/my_write.c
+++ Level 3/ft_atoi_base/my_write.c
@@ -1,24 +1,32 @@
-int		ft_atoi_base(const char *str, int str_base)
+int ft_atoi_base(const char *str, int str_base)
 {
-	int i = 0
+	int i = 0;
+	int res = 0;
 	int n = 1;
-	int result = 0;
-
-	if (str[i] == '-')
+​
+	while (str[i] <= 32)
+		i++;
+	if (str[i] == '-'||str[i] == '+')
 	{
-		n = -1;
+		if (str[i] == '-')
+			n = -1;
 		i++;
 	}
-	while (str[i])
+	if (str[i] && str[i] >= '0' && str[i] <= '9')
 	{
-		result *= str_base;
-		if (str[i] >= '0' && str[i] <= '9')
-			result += str[i] - 48;
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			result += str[i] - 55;
-		else if (str[i] >= 'a' && str[i] <= 'z')
-			result += str[i] - 87;
-		i++;
+		res *= str_base;
+		res += str[i] - 48;
 	}
-	return (result * n);
+	else if (str[i] && str[i] >= 'A' && str[i] <= 'Z')
+	{
+		res *= str_base;
+		res += str[i] - 55;
+	}
+	else if (str[i] && str[i] >= 'a' && str[i] <= 'z')
+	{
+		res *= str_base;
+		res += str[i] - 87;
+	}
+	i++;
+	return res * n;
 }
