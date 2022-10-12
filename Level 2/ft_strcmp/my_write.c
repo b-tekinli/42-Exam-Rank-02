efc9c077b2cc6c1b2a5a30164ee6d7df989b8bc4
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-10-12 06:37:20 +0000

    ok

--- Level 2/ft_strcmp/my_write.c
+++ Level 2/ft_strcmp/my_write.c
@@ -1,6 +1,9 @@
-int    ft_strcmp(char *s1, char *s2) {
+int ft_strcmp(char *s1, char *s2) {
     int i = 0;
-    while (s2[i] && s1[i] && s2 == s1)
+
+    while (s2[i] && s1[i] && s2[i] == s1[i])
+    {
         i++;
+    }
     return (s1[i] - s2[i]);
 }
