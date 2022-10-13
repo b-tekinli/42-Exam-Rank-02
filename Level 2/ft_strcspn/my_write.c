f828fa504b288ffa70f2e55f0b886395510c36b3
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-10-13 23:35:21 +0300

    ok

--- Level 2/ft_strcspn/my_write.c
+++ Level 2/ft_strcspn/my_write.c
@@ -25,3 +25,26 @@ size_t	ft_strcspn(const char *s, const char *reject) {
     
     return (i);
 }
+
+/*
+size_t  ft_strcspn(const char *s, const char *reject)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (reject[j])
+		{
+			if (reject[j] == s[i])
+				return (i);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (i);
+}
+*/
