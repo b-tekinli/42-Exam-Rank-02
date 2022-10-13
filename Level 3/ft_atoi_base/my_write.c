2df2c338e15406e13f13aa6ca3f14778be317697
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-10-13 07:57:17 +0000

    ok

--- Level 3/ft_atoi_base/my_write.c
+++ Level 3/ft_atoi_base/my_write.c
@@ -1,32 +1,43 @@
-int ft_atoi_base(const char *str, int str_base)
+int ft_isblank(char c)
 {
-	int i = 0;
-	int res = 0;
-	int n = 1;
-​
-	while (str[i] <= 32)
-		i++;
-	if (str[i] == '-'||str[i] == '+')
-	{
-		if (str[i] == '-')
-			n = -1;
-		i++;
-	}
-	if (str[i] && str[i] >= '0' && str[i] <= '9')
-	{
-		res *= str_base;
-		res += str[i] - 48;
-	}
-	else if (str[i] && str[i] >= 'A' && str[i] <= 'Z')
-	{
-		res *= str_base;
-		res += str[i] - 55;
-	}
-	else if (str[i] && str[i] >= 'a' && str[i] <= 'z')
-	{
-		res *= str_base;
-		res += str[i] - 87;
-	}
-	i++;
-	return res * n;
+	if (c <= 32)
+		return (1);
+	return (0);
+}
+
+int		ft_isvalid(char c, int base)
+{
+	char digits[17] = "0123456789abcdef";
+	char digits2[17] = "0123456789ABCDEF";
+
+	while (base--)
+		if (digits[base] == c || digits2[base] == c)
+			return (1);
+	return (0);
+}
+
+int		ft_value_of(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (c - '0');
+	else if (c >= 'a' && c <= 'f')
+		return (c - 'a' + 10);
+	else if (c >= 'A' && c <= 'F')
+		return (c - 'A' + 10);
+	return (0);
+}
+
+int		ft_atoi_base(const char *str, int str_base)
+{
+	int result;
+	int sign;
+
+	result = 0;
+	while (ft_isblank(*str))
+		str++;
+	sign = (*str == '-') ? -1 : 1;
+	(*str == '-' || *str == '+') ? ++str : 0;
+	while (ft_isvalid(*str, str_base))
+		result = result * str_base + ft_value_of(*str++);
+	return (result * sign);
 }
