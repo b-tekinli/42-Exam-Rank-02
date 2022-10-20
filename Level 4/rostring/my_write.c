d12b8592bf4f2f473fab4284f50972976cfb80d2
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-10-20 23:03:59 +0300

    ok

--- Level 4/rostring/my_write.c
+++ Level 4/rostring/my_write.c
@@ -17,7 +17,7 @@ int main(int ac, char **av)
 		end = i;
 		while(av[1][i] == ' ' || av[1][i] == '\t')
 			i++;
-		while(av[1][i] != '\0')
+		while(av[1][i])
 		{
 			flag = 1;
 			write (1, &av[1][i], 1);
