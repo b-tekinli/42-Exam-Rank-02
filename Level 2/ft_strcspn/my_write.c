c33831a4cc2ee723a7f20fc47a6ee5f25ade09b3
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-01-04 21:16:38 +0300

    Update my_write.c

--- Level 2/ft_strcspn/my_write.c
+++ Level 2/ft_strcspn/my_write.c
@@ -5,46 +5,17 @@ size_t	ft_strcspn(const char *s, const char *reject) {
     int j = 0;
 
     while (s[i])
-    {
         j = 0;
         while (reject[j])
-        {
             if (s[i] == reject[j])
-            {
                 return (i);  
-            }
             j++;
-        }
         i++;
-    }
+	
+	
     i = 0;
     while (s[i])
-    {
         i++;
-    }
-    
+ 
     return (i);
 }
-
-/*
-size_t  ft_strcspn(const char *s, const char *reject)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	while (s[i])
-	{
-		while (reject[j])
-		{
-			if (reject[j] == s[i])
-				return (i);
-			j++;
-		}
-		j = 0;
-		i++;
-	}
-	return (i);
-}
-*/
