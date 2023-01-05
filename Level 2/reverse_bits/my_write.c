b31345d582b7ab2c6a8a5ab45901e640bc8119e3
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-01-05 20:08:08 +0300

    ok

--- Level 2/reverse_bits/my_write.c
+++ Level 2/reverse_bits/my_write.c
@@ -1,10 +1,9 @@
-unsigned char	reverse_bits(unsigned char octet) {
-    return (((octet >> 0) & 1) << 7) | \
-            (((octet >> 0) & 1) << 6) | \
-            (((octet >> 0) & 1) << 5) | \
-            (((octet >> 0) & 1) << 4) | \
-            (((octet >> 0) & 1) << 3) | \
-            (((octet >> 0) & 1) << 2) | \
-            (((octet >> 0) & 1) << 1) | \
-            (((octet >> 0) & 1) << 0);
+unsigned char reverse_bits(unsigned char octet) {
+  unsigned char result = 0;
+    
+  for (int i = 0; i < 8; i++) {
+    result = (result << 1) | (octet & 1);
+    octet >>= 1;
+  }
+  return result;
 }
