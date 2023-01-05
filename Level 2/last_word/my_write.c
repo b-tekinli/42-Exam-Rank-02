4868d3d87efe3ead55e4938af83a604f1e5cc87f
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-01-05 15:36:22 +0300

    ok

--- Level 2/last_word/my_write.c
+++ Level 2/last_word/my_write.c
@@ -1,27 +1,64 @@
 #include <unistd.h>
 
-void	last_word(char *str)
+int	blank(char c)
 {
-	int i = 0;
-	int	j = 0;
-	
-	while (str[i])
-	{
-		if ((str[i] == ' ' || str[i] == '\0' || str[i] == '\t') && str[i + 1] >= 33 && str[i + 1] <= 126)
-			j = i + 1;
-		i++;
-	}
-	while (str[j] >= 33 && str[j] <= 127)
-	{
-		write(1, &str[j], 1);
-		j++;
-	}
+	if (c == ' ' || c == '\t')
+		return(1);
+
+	return(0);
 }
 
-int		main(int ac, char **av)
+int	main(int ac, char **av)
 {
+	unsigned int	i;
+
+	i = 0;
 	if (ac == 2)
-		last_word(av[1]);
+	{
+		while (av[1][i])
+			i++;
+		i--;
+		while (i > 0 && blank(av[1][i]))
+			i--;
+		while (i > 0 && !blank(av[1][i]))
+			i--;
+		while (av[1][i] && blank(av[1][i]))
+			i++;
+		while (av[1][i] && !blank(av[1][i]))
+		{
+			write(1, &av[1][i], 1);
+			i++;
+		}
+	}
 	write(1, "\n", 1);
-	return (0);
+	return(0);
 }
+
+
+
+// void last_word(char *str) {
+//     int i = 0;
+//     int j = 0;
+
+//     while (str[i]) {
+//         if ((str[i] == ' ' || str[i] == '\0' || str[i] == '\t') && |
+//         str[i + 1] >= 33 && str[i + 1] <= 126) {
+//             j = i + 1;
+//         }
+//         i++;
+//     }
+
+//     while (str[j] >= 33 && str[j] <= 127) {
+//         write(1, &str[j], 1);
+//         j++;
+//     }
+// }
+
+// int main(int ac, char **av) {
+//     if (ac == 2) {
+//         last_word(av[1]);
+//     }
+//     write(1, "\n", 1);
+//     return (0);
+// }
+
