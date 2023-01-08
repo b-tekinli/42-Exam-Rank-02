ecede38d7453448f1b34c64b9301e04fe27815bd
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-01-08 15:01:07 +0300

    ok

--- Level 1/rotone/my_write.c
+++ Level 1/rotone/my_write.c
@@ -6,7 +6,7 @@ int main(int ac, char **av) {
     if(ac == 2) {
         while (av[1][i])
         {
-            if ((av[1][i] >= 'A' && av[1][i] <= 'Y') || av[1][i] >= 'a' && av[1][i] <= 'y')
+            if ((av[1][i] >= 'A' && av[1][i] <= 'Y') || (av[1][i] >= 'a' && av[1][i] <= 'y'))
             {
                 av[1][i]++;
             }
